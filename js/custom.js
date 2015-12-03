49859f71c8a230508af34131f90043db3960e2e6
martinkaru <martinkaru@gmail.com>
2015-12-03 02:00:52 +0200

    Donuts

--- js/custom.js
+++ js/custom.js
@@ -4,55 +4,73 @@
 requirejs(['d3js'], function () {
     'use strict';
 
-    var width = 960,
-        height = 500,
-        radius = Math.min(width, height) / 2;
+    // Defaults
+    var width = 300,
+        height = 300,
+        radius = Math.min(width, height) / 2,
+        outerRadius = radius - 10,
+        innerRadius = radius - 20;
 
-    var color = d3.scale.ordinal()
-        .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
-
-    var arc = d3.svg.arc()
-        .outerRadius(radius - 10)
-        .innerRadius(radius - 70);
-
-    var pie = d3.layout.pie()
-        .sort(null)
-        .value(function (d) {
-            return d.population;
-        });
-
-    var svg = d3.select("body").append("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .append("g")
-        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-    d3.csv("data/donut.csv", type, function (error, data) {
-        if (error) throw error;
-
-        var g = svg.selectAll(".arc")
-            .data(pie(data))
-            .enter().append("g")
-            .attr("class", "arc");
-
-        g.append("path")
-            .attr("d", arc)
-            .style("fill", function (d) {
-                return color(d.data.age);
-            });
-
-        g.append("text")
-            .attr("transform", function (d) {
-                return "translate(" + arc.centroid(d) + ")";
-            })
-            .attr("dy", ".35em")
-            .text(function (d) {
-                return d.data.age;
-            });
-    });
-
-    function type(d) {
+    /**
+     * Make sure the elements' values are positive
+     * @param d
+     * @returns {*}
+     */
+    function normalize(d) {
         d.population = +d.population;
         return d;
     }
+
+    /**
+     * Attach D3 donut to elements
+     */
+    function attachDonut(elementName, dataLocation, color) {
+        var arc = d3.svg.arc()
+            .outerRadius(outerRadius)
+            .innerRadius(innerRadius);
+
+        var pie = d3.layout.pie()
+            .sort(null)
+            .value(function (d) {
+                return d.value;
+            });
+
+        var svg = d3.select(elementName).append("svg")
+            .attr("width", width)
+            .attr("height", height)
+            .append("g")
+            .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+        d3.csv(dataLocation, normalize, function (error, data) {
+            if (error) throw error;
+
+            var g = svg.selectAll(".arc")
+                .data(pie(data))
+                .enter().append("g")
+                .attr("class", "arc");
+
+            g.append("path")
+                .attr("d", arc)
+                .style("fill", function (d) {
+                    return color(d.data.key);
+                })
+                .style('stroke-width', 0);
+
+            g.append("text")
+                .attr("transform", function (d) {
+                    return "translate(" + arc.centroid(d) + ")";
+                })
+                .attr("dy", ".35em")
+            //.text(function (d) {
+            //    return d.data.age;
+            //});
+        });
+    }
+
+    attachDonut('div.donut01', 'data/donut0001.csv', d3.scale.ordinal()
+        .range(["#49d025", "#005d00"]));
+    attachDonut('div.donut02', 'data/donut0002.csv', d3.scale.ordinal()
+        .range(["#03c4e2", "#00465c"]));
+    attachDonut('div.donut03', 'data/donut0003.csv', d3.scale.ordinal()
+        .range(["#febf00", "#d34d0f"]));
 });
